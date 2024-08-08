71da2a9a2858e96ed4c6ee5cfa8dd370c0436382
hubirto <hubert.rybicki.hr@gmail.com>
2024-08-08 19:02:17 +0200

    init

---
+++ src/main/java/pl/pomoku/craftclusterapi/CraftClusterApiApplication.java
@@ -0,0 +1,13 @@
+package pl.pomoku.craftclusterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftClusterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraftClusterApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pomoku/craftclusterapi/CraftClusterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pomoku.craftclusterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftClusterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
